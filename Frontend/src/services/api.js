5542c850d4590841989ed0be4b31f87ea24dc6f1
zaidmirza24 <mirzamohdzaid72@gmail.com>
2025-09-06 17:11:19 +0530

    api url fixed

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://verifyme-uw2l.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
